f7288e9721e8a336c7d53d56cd052ad91f71bf19
maximbond2090-byte <maximbond2090@gmail.com>
2026-05-12 21:28:24 +0300

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,24 @@
+console.log("BMW сайт працює");
+
+
+//document.addEventListener("DOMContentLoaded", () => {
+//    document.body.classList.add("fade-in");
+//});
+//
+//document.querySelectorAll("a").forEach(link => {
+ //   link.addEventListener("click", function(e) {
+ //       const href = this.getAttribute("href");
+//
+ //       // тільки для внутрішніх сторінок
+ //       if (href && !href.startsWith("#")) {
+ //           e.preventDefault();
+//
+ //           document.body.classList.remove("fade-in");
+ //           document.body.classList.add("fade-out");
+//
+ //           setTimeout(() => {
+ //               window.location.href = href;
+ //           }, 20);
+ //       }
+ //   });
+//});
